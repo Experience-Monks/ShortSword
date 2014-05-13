d37fd9083880c27cbec91e0175bb0d1040f75af3
Tomasz Dysinski <tom@jam3.com>
2014-05-13 15:21:29 -0400

    cleanup

--- src/utils/PerformanceGraph.js
+++
@@ -1,9 +0,0 @@
-var CanvasGraph = require('./CanvasGraph');
-
-function PerformanceGraph(props) {
-	CanvasGraph.call(this, props);	
-};
-
-PerformanceGraph.prototype = Object.create(CanvasGraph.prototype);
-
-module.exports = PerformanceGraph;
