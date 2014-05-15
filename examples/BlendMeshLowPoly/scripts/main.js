0435b8132bdc7b3a9589fe349599e68a8e9b3894
Tomasz Dysinski <tom@jam3.com>
2014-05-15 18:53:04 -0400

    optimized octtreesort by unrolling the axes

--- examples/BlendMeshLowPoly/scripts/main.js
+++ examples/BlendMeshLowPoly/scripts/main.js
@@ -7,12 +7,13 @@ var totalVerts = 100000;
 var _this = this;
 SHORTSWORD.Loader.loadGeometryOBJ("../assets/models/cube.obj", function(geometry1) {
 	SHORTSWORD.Loader.loadGeometryOBJ("../assets/models/yoshi.obj", function(geometry2) {
-		SHORTSWORD.GeometryUtils.fillSurfaces(geometry1, totalVerts);
-		SHORTSWORD.GeometryUtils.fillSurfaces(geometry2, totalVerts);
-		var mesh = new SHORTSWORD.BlendMesh(geometry1, geometry2);
-		ssView.scene.add(mesh);
-		_this.blendModel = mesh;
-		_this.blendModel.scale.multiplyScalar(60);
+		SHORTSWORD.GeometryUtils.fillSurfaces(geometry1, geometry2.vertices.length);
+		SHORTSWORD.GeometryGarage.fillSurfaces([geometry1, geometry2], totalVerts, function(){
+			var mesh = new SHORTSWORD.BlendMesh(geometry1, geometry2);
+			ssView.scene.add(mesh);
+			_this.blendModel = mesh;
+			_this.blendModel.scale.multiplyScalar(60);
+		});
 	});
 });
 
@@ -21,6 +22,7 @@ var canvasGraph = new SHORTSWORD.CanvasGraph();
 canvasGraph.addValue(SHORTSWORD.FPS, "fps", "green", "FPS Smoothed");
 
 ssView.renderManager.onEnterFrame.add(function() {
+	SHORTSWORD.GeometryGarage.doSomeWork();
 	if(!_this.blendModel) return;
 	_this.blendModel.blend = Math.sin((new Date()).getTime() * .001) * .5 + .5;
 	_this.blendModel.rotateY(mouseMove.x * mouseMove.speed);
--- src/utils/GeometryGarage.js
+++ src/utils/GeometryGarage.js
@@ -6,17 +6,22 @@ var GeometryGarage = {
 		var verticesPerWorkRun = 10000;
 		var targetWorkRunDuration = 6;
 		var targetTweakRatio = 1.2;
-		return function(geometry, newTotalVertices, callback) {
+		return function(geometries, newTotalVertices, callback) {
+			if(!GeometryUtils.checkIfGeometryAttributesLengthsMatch(geometries)) {
+				return;
+			}
 			var workLoad = {
 				timeToProcess: 0,
 				done: false,
-				currentTotalVertices: geometry.vertices.length,
+				currentTotalVertices: geometries[0].vertices.length,
 				targetTotalVertices: newTotalVertices,
 				callback: callback,
 				run: function() {
 					var timeBefore = new Date;
 					var nextTotal = Math.min(this.targetTotalVertices, this.currentTotalVertices + verticesPerWorkRun);
-					GeometryUtils.fillSurfaces(geometry, nextTotal);
+					for (var i = 0; i < geometries.length; i++) {
+						GeometryUtils.fillSurfaces(geometries[i], nextTotal);
+					};
 					this.currentTotalVertices = nextTotal;
 					var timeAfter = new Date;
 					var duration = timeAfter - timeBefore;
@@ -35,6 +40,9 @@ var GeometryGarage = {
 			work.push(workLoad);
 		};
 	}(),
+	octTreeSort: function() {
+		
+	},
 	doSomeWork: function() {
 		for (var i = work.length - 1; i >= 0; i--) {
 			work[i].run();
